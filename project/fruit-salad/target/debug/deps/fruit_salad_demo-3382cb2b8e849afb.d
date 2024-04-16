6084875ad3ea47f60ae5426f02048a58f09a2d13
Muhammad Hanif Fajari <105597377+mhaniffajari@users.noreply.github.com>
2024-04-16 08:52:36 +0000

    week 1: fixing makefile

---
+++ project/fruit-salad/target/debug/deps/fruit_salad_demo-3382cb2b8e849afb.d
@@ -0,0 +1,5 @@
+/workspaces/rust-data-engineer/project/fruit-salad/target/debug/deps/fruit_salad_demo-3382cb2b8e849afb: src/main.rs
+
+/workspaces/rust-data-engineer/project/fruit-salad/target/debug/deps/fruit_salad_demo-3382cb2b8e849afb.d: src/main.rs
+
+src/main.rs:
